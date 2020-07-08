66349dc688469810785019c35c000d67f65fc27a
npnelson <nnelson1@insight.rr.com>
2020-07-08 15:34:29 -0400

    WIP

---
+++ src/NetToolBox.CodeAuthentication/CodeAuthenticationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace NetToolBox.CodeAuthentication
+{
+    public sealed class CodeAuthenticationHandler : AuthenticationHandler<CodeAuthenticationSchemeOptions>
+    {
+        public CodeAuthenticationHandler(IOptionsMonitor<CodeAuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var code = Request.Query["code"];
+            if (this.Options.ValidCodes.Contains(code))
+            {
+                var identity = new ClaimsIdentity(Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+            }
+            else
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid authentication code"));
+            }
+        }
+    }
+}
---
+++ test/CodeAuthenticationHandler.Tests/CodeAuthenticationHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NetToolBox.CodeAuthentication;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeAuthenticationHandler.Tests
+{
+    public sealed class CodeAuthenticationHandlerTests
+    {
+        [Theory]
+        [InlineData("invalidCode", false)]
+        [InlineData("validcode1", true)]
+        [InlineData("validcode2", true)]
+        public async Task ValidateCodesTest(string code, bool succeeded)
+        {
+            var fixture = new CodeAuthenticationTestFixture(new List<string> { "validcode1", "validcode2" });
+            var handler = fixture.Handler;
+            var context = new DefaultHttpContext();
+            context.Request.QueryString = new QueryString($"?code={code}");
+            await handler.InitializeAsync(new AuthenticationScheme("CodeAuthentication", null, typeof(NetToolBox.CodeAuthentication.CodeAuthenticationHandler)), context);
+            var result = await handler.AuthenticateAsync();
+            result.Succeeded.Should().Be(succeeded);
+        }
+
+    }
+
+    public sealed class CodeAuthenticationTestFixture
+    {
+        private readonly Mock<IOptionsMonitor<CodeAuthenticationSchemeOptions>> _options = new Mock<IOptionsMonitor<CodeAuthenticationSchemeOptions>>();
+        private readonly Mock<ILoggerFactory> _loggerFactory = new Mock<ILoggerFactory>();
+        private readonly Mock<UrlEncoder> _encoder = new Mock<UrlEncoder>();
+        private readonly Mock<ISystemClock> _clock = new Mock<ISystemClock>();
+
+        public NetToolBox.CodeAuthentication.CodeAuthenticationHandler Handler;
+        public CodeAuthenticationTestFixture(List<string> validCodes)
+        {
+            var options = new CodeAuthenticationSchemeOptions { ValidCodes = validCodes };
+            _options.Setup(x => x.Get(It.IsAny<string>())).Returns(options);
+            var logger = new Mock<ILogger<NetToolBox.CodeAuthentication.CodeAuthenticationHandler>>();
+            _loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+            Handler = new NetToolBox.CodeAuthentication.CodeAuthenticationHandler(_options.Object, _loggerFactory.Object, _encoder.Object, _clock.Object);
+        }
+    }
+}
