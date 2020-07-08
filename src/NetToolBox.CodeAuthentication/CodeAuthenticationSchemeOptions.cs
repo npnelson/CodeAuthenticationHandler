31fcff1a98a6f69a760026b237f794d8757f0c57
Nicholas P Nelson <nnelson1@insight.rr.com>
2020-07-08 15:47:29 -0400

    Initial Commit (#1)

---
+++ src/NetToolBox.CodeAuthentication/CodeAuthenticationSchemeOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Collections.Generic;
+
+namespace NetToolBox.CodeAuthentication
+{
+    public sealed class CodeAuthenticationSchemeOptions : AuthenticationSchemeOptions
+    {
+        public List<string> ValidCodes { get; set; } = null!;
+    }
+}
---
+++ src/NetToolBox.CodeAuthentication/CodeAuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using NetToolBox.CodeAuthentication;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CodeAuthenticationServiceCollectionExtensions
+    {
+        private const string DefaultCodeAuthenticationName = "CodeAuthentication";
+        /// <summary>
+        /// Adds CodeAuthenticationHandler (i.e. ?code=(password))
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="codeAuthenticationName">The name you want to use for the authentication scheme</param>
+        /// <param name="configurationSection">A configuration section containing a list of the valid codes for this scheme</param>
+        public static void AddCodeAuthentication(this IServiceCollection services, string codeAuthenticationName, IConfigurationSection configurationSection)
+        {
+            services.AddAuthentication(codeAuthenticationName).AddScheme<CodeAuthenticationSchemeOptions, CodeAuthenticationHandler>(codeAuthenticationName, options => options.ValidCodes = configurationSection.Get<List<string>>());
+        }
+
+        /// <summary>
+        /// Adds CodeAuthenticationHandler (i.e. ?code=(password)) with the default scheme name of "CodeAuthentication"
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configurationSection">A configuration section containing a list of the valid codes for this scheme</param>
+        public static void AddCodeAuthentication(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            services.AddAuthentication(DefaultCodeAuthenticationName).AddScheme<CodeAuthenticationSchemeOptions, CodeAuthenticationHandler>(DefaultCodeAuthenticationName, options => options.ValidCodes = configurationSection.Get<List<string>>());
+        }
+    }
+}
